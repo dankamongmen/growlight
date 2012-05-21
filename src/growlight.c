b4c42c317bb41bd6e465b0dc79be198c4a10ed3e
nick black <dank@qemfd.net>
2012-05-21 03:32:33 -0400

    get the disk entries by path

--- src/growlight.c
+++ src/growlight.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdio.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 #include <src/config.h>
@@ -18,16 +19,35 @@ inotify_fd(void){
 }
 
 static inline int
-watch_dir(int fd,const char *dir){
-	int wfd;
+watch_dir(int fd,const char *dfp){
+	struct dirent *d;
+	int wfd,dfd;
+	DIR *dir;
 
-	wfd = inotify_add_watch(fd,dir,IN_CREATE|IN_DELETE|IN_MOVED_FROM|IN_MOVED_TO);
+	wfd = inotify_add_watch(fd,dfp,IN_CREATE|IN_DELETE|IN_MOVED_FROM|IN_MOVED_TO);
 	if(wfd < 0){
-		fprintf(stderr,"Coudln't get inotify watch (%s?)\n",strerror(errno));
+		fprintf(stderr,"Coudln't inotify on %s (%s?)\n",dfp,strerror(errno));
 		return -1;
 	}else{
-		printf("Watching %s on %d\n",dir,wfd);
+		printf("Watching %s on %d\n",dfp,wfd);
 	}
+	if((dir = opendir(dfp)) < 0){
+		fprintf(stderr,"Coudln't open %s (%s?)\n",dfp,strerror(errno));
+		inotify_rm_watch(fd,wfd);
+		return -1;
+	}
+	if((dfd = dirfd(dir)) < 0){
+		fprintf(stderr,"Coudln't get fd on %s (%s?)\n",dfp,strerror(errno));
+		inotify_rm_watch(fd,wfd);
+		closedir(dir);
+		return -1;
+	}
+	while( (d = readdir(dir)) ){
+		if(d->d_type == DT_LNK){
+			printf("Link: %s\n",d->d_name);
+		}
+	}
+	closedir(dir);
 	return 0;
 }
 
