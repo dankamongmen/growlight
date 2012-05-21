bd5b06e9ae3036c53b0927696ad37e642da87958
nick black <dank@qemfd.net>
2012-05-21 03:55:22 -0400

    build up a device table

--- src/growlight.c
+++ src/growlight.c
@@ -9,6 +9,50 @@
 
 #define DISKS_BY_PATH "/dev/disk/by-path/"
 
+// An (non-link) entry in the device hierarchy, representing a disk or
+// partition.
+typedef struct device {
+	struct device *next;
+	char name[PATH_MAX];
+	char path[PATH_MAX];
+	char id[PATH_MAX];
+	char label[PATH_MAX];
+	int major,minor;
+} device;
+
+static device *devs;
+
+static inline device *
+create_new_device(const char *name){
+	device *d;
+
+	if(strlen(name) >= sizeof(d->name)){
+		return NULL;
+	}
+	if( (d = malloc(sizeof(*d))) ){
+		memset(d,0,sizeof(*d));
+		strcpy(d->name,name);
+		// FIXME get major/minors
+	}
+	return d;
+}
+
+static device *
+lookup_device(const char *name){
+	device *d;
+
+	for(d = devs ; d ; d = d->next){
+		if(strcmp(name,d->name) == 0){
+			return d;
+		}
+	}
+	if( (d = create_new_device(name)) ){
+		d->next = devs;
+		devs = d;
+	}
+	return d;
+}
+
 static inline int
 inotify_fd(void){
 	int fd;
@@ -22,7 +66,7 @@ inotify_fd(void){
 static inline int
 watch_dir(int fd,const char *dfp){
 	struct dirent *d;
-	int wfd,dfd;
+	int wfd,dfd,r;
 	DIR *dir;
 
 	wfd = inotify_add_watch(fd,dfp,IN_CREATE|IN_DELETE|IN_MOVED_FROM|IN_MOVED_TO);
@@ -43,7 +87,9 @@ watch_dir(int fd,const char *dfp){
 		closedir(dir);
 		return -1;
 	}
-	while( (d = readdir(dir)) ){
+	r = 0;
+	while( errno = 0, (d = readdir(dir)) ){
+		r = -1;
 		if(d->d_type == DT_LNK){
 			char buf[PATH_MAX];
 
@@ -51,12 +97,24 @@ watch_dir(int fd,const char *dfp){
 				fprintf(stderr,"Couldn't read link at %s/%s (%s?)\n",
 					dfp,d->d_name,strerror(errno));
 			}else{
+				const device *dev;
+
+				if((dev = lookup_device(d->d_name)) == NULL){
+					fprintf(stderr,"Couldn't look up %s (%s?)\n",
+							d->d_name,strerror(errno));
+					break;
+				}
 				printf("%s -> %s\n",d->d_name,buf);
 			}
 		}
+		r = 0;
+	}
+	if(errno){
+		fprintf(stderr,"Error reading %s (%s?)\n",dfp,strerror(errno));
+		r = -1;
 	}
 	closedir(dir);
-	return 0;
+	return r;
 }
 
 int main(void){
