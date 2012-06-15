055a9dc0b209af198235a40bc2df81cd182db860
nick black <dank@qemfd.net>
2012-06-15 13:51:26 -0400

    kill cruft

--- src/mbr.c
+++ src/mbr.c
@@ -17,7 +17,6 @@
 
 int mbrsha1(int fd,void *buf){
 	unsigned char mbr[MBR_CODE_SIZE];
-	unsigned z;
 	ssize_t r;
 
 	lseek(fd,0,SEEK_SET);
@@ -27,12 +26,6 @@ int mbrsha1(int fd,void *buf){
 	if(r < (int)sizeof(mbr)){
 		return -1;
 	}
-	printf("%02x %02x\n",mbr[0],mbr[1]);
-	for(z = 0 ; z < sizeof(mbr) ; ++z){
-		if(mbr[z]){
-			printf("byte at %u\n",z); break;
-		}
-	}
 	if(SHA1(mbr,sizeof(mbr),buf) == NULL){
 		fprintf(stderr,"Couldn't perform SHA1 for %d (%s)\n",fd,ERR_lib_error_string(ERR_get_error()));
 	}
@@ -41,16 +34,6 @@ int mbrsha1(int fd,void *buf){
 
 int zerombrp(const void *buf){
 	const void *z = "\x63\x9a\xc5\xcd\xf8\xa5\xcf\x32\x45\x97\x59\x32\xc6\xa4\x21\x54\x50\xa7\xb9\x8f";
-	int r;
 
-	for(r = 0 ; r < 20 ; ++r){
-		printf("\\x%02x",((const unsigned char*)buf)[r]);
-	}
-	printf("\n");
-	for(r = 0 ; r < 20 ; ++r){
-		printf("\\x%02x",((const unsigned char *)z)[r]);
-	}
-	r = !memcmp(buf,z,20);
-	printf("*&************* %x %02x %02x\n",r,((const unsigned char *)buf)[1],((const unsigned char *)buf)[18]);
-	return r;
+	return !memcmp(buf,z,20);
 }
