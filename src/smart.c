b82547aee09376a982fbb033d79c28837add4dba
nick black <dank@qemfd.net>
2012-06-19 09:46:48 -0400

    smart files whoops

---
+++ src/smart.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdio.h>
+#include <sysfs.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <atasmart.h>
+
+#include <smart.h>
+#include <growlight.h>
+
+int probe_smart(device *d){
+	char path[PATH_MAX];
+	SkDisk *sk;
+
+	if(snprintf(path,sizeof(path),"/dev/%s",d->name) >= (int)sizeof(path)){
+		fprintf(stderr,"Bad name: %s\n",d->name);
+		return -1;
+	}
+	sk = NULL;
+	if(sk_disk_open(path,&sk)){
+		verbf("Couldn't probe %s SMART\n",path);
+		return -1;
+	}
+	d->blkdev.smart = 1;
+	sk_disk_free(sk);
+	return 0;
+}
---
+++ src/smart.h
@@ -0,0 +1,16 @@
+#ifndef GROWLIGHT_SMART
+#define GROWLIGHT_SMART
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct device;
+
+int probe_smart(struct device *d);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
