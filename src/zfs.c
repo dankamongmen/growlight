f54e216cbfccaad6d25681197fcaa2edce3ebf4b
nick black <dank@qemfd.net>
2012-06-20 17:05:45 -0400

    successfully run zprop_get_list() with default properties

--- src/zfs.c
+++ src/zfs.c
@@ -18,9 +18,34 @@
 #include <libzfs.h>
 
 static libzfs_handle_t *zht;
+static char history[HIS_MAX_RECORD_LEN];
+// FIXME taken from zpool.c source
+static char props[] = "name,size,allocated,free,capacity,dedupratio,health,altroot";
+
+static int
+scan_zpools(libzfs_handle_t *zfs){
+	zprop_list_t *pools = NULL;
+
+	if(zprop_get_list(zfs,props,&pools,ZFS_TYPE_POOL)){
+		fprintf(stderr,"Coudln't list ZFS pools\n");
+		return -1;
+	}
+	zprop_free_list(pools);
+	return 0;
+}
 
 int init_zfs_support(void){
 	if((zht = libzfs_init()) == NULL){
+		return -1;
+	}
+	libzfs_print_on_error(zht,true);
+	zpool_set_history_str(PACKAGE, 0, NULL, history);
+	if(zpool_stage_history(zht,history)){
+		fprintf(stderr,"ZFS history didn't match!\n");
+		return -1;
+	}
+	if(scan_zpools(zht)){
+		stop_zfs_support();
 		return -1;
 	}
 	verbf("Initialized ZFS support.\n");
@@ -28,8 +53,10 @@ int init_zfs_support(void){
 }
 
 int stop_zfs_support(void){
-	libzfs_fini(zht);
-	zht = NULL;
+	if(zht){
+		libzfs_fini(zht);
+		zht = NULL;
+	}
 	return 0;
 }
 
