25f67528e102ab549b9b4390a93e8a2ac6cc6626
nick black <dank@qemfd.net>
2012-06-27 00:39:30 -0400

    start in on the ncurses UI

--- src/ncurses.c
+++ src/ncurses.c
@@ -1,14 +1,228 @@
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "config.h"
 #include "growlight.h"
 
+#ifdef HAVE_NCURSESW_H
+#include <term.h>
+#include <ncurses.h>
+#else
+#ifdef HAVE_NCURSESW_CURSES_H
+#include <ncursesw/term.h>
+#include <ncursesw/curses.h>
+#else
+#error "Couldn't find working cursesw headers"
+#endif
+#endif
+
+// Our color pairs
+enum {
+	BORDER_COLOR = 1,		// Main window
+	HEADER_COLOR,
+	FOOTER_COLOR,
+};
+
+static int
+bevel(WINDOW *w){
+	static const cchar_t bchr[] = {
+		{ .attr = 0, .chars = L"╭", },
+		{ .attr = 0, .chars = L"╮", },
+		{ .attr = 0, .chars = L"╰", },
+		{ .attr = 0, .chars = L"╯", },
+	};
+	int rows,cols;
+
+	getmaxyx(w,rows,cols);
+	assert(rows && cols);
+	// called as one expects: 'mvwadd_wch(w,rows - 1,cols - 1,&bchr[3]);'
+	// we get ERR returned. this is known behavior: fuck ncurses. instead,
+	// we use mvwins_wch, which doesn't update the cursor position.
+	// see http://lists.gnu.org/archive/html/bug-ncurses/2007-09/msg00001.ht
+	assert(mvwadd_wch(w,0,0,&bchr[0]) != ERR);
+	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
+	assert(mvwins_wch(w,0,cols - 1,&bchr[1]) != ERR);
+	assert(mvwvline(w,1,cols - 1,ACS_VLINE,rows - 1) != ERR);
+	assert(mvwvline(w,1,0,ACS_VLINE,rows - 1) != ERR);
+	assert(mvwadd_wch(w,rows - 1,0,&bchr[2]) != ERR);
+	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
+	assert(mvwins_wch(w,rows - 1,cols - 1,&bchr[3]) != ERR);
+	return OK;
+}
+
+static int
+ncurses_cleanup(WINDOW **w){
+	int ret = 0;
+
+	if(*w){
+		if(delwin(*w) != OK){
+			ret = -1;
+		}
+		*w = NULL;
+	}
+	if(stdscr){
+		if(delwin(stdscr) != OK){
+			ret = -2;
+		}
+		stdscr = NULL;
+	}
+	if(endwin() != OK){
+		ret = -3;
+	}
+	switch(ret){
+	case -3: fprintf(stderr,"Couldn't end main window\n"); break;
+	case -2: fprintf(stderr,"Couldn't delete main window\n"); break;
+	case -1: fprintf(stderr,"Couldn't delete main pad\n"); break;
+	case 0: break;
+	default: fprintf(stderr,"Couldn't cleanup ncurses\n"); break;
+	}
+	return ret;
+}
+
+static char statusmsg[80];
+static unsigned count_adapters;
+
+#define START_COL 1		// Room to leave for borders
+
+static int
+draw_main_window(WINDOW *w){
+	int rows,cols,scol;
+
+	getmaxyx(w,rows,cols);
+	assert(wattrset(w,A_DIM | COLOR_PAIR(BORDER_COLOR)) != ERR);
+	if(bevel(w) != OK){
+		goto err;
+	}
+	// FIXME move this over! it is ugly on the left, clashing with ifaces
+	// 5 for 0-offset, '[', ']', and 2 spaces on right side.
+	// 5 for '|', space before and after, and %2d-formatted integer
+	scol = cols - 5 - __builtin_strlen(PACKAGE) - 1 - __builtin_strlen(VERSION)
+		- 5 - __builtin_strlen("adapters" - (count_adapters != 1));
+	assert(mvwprintw(w,0,scol,"[") != ERR);
+	assert(wattron(w,A_BOLD | COLOR_PAIR(HEADER_COLOR)) != ERR);
+	assert(wprintw(w,"%s %s | %d iface%s",PACKAGE,VERSION,
+			count_adapters,count_adapters == 1 ? "" : "s") != ERR);
+	assert(wattrset(w,COLOR_PAIR(BORDER_COLOR)) != ERR);
+	assert(wprintw(w,"]") != ERR);
+	assert(wattron(w,A_BOLD | COLOR_PAIR(FOOTER_COLOR)) != ERR);
+	// addstr() doesn't interpret format strings, so this is safe. It will
+	// fail, however, if the string can't fit on the window, which will for
+	// instance happen if there's an embedded newline.
+	mvwaddstr(w,rows - 1,START_COL * 2,statusmsg); // FIXME
+	//assert(mvwaddwstr(w,rows - 1,START_COL * 2,statusmsg) != ERR);
+	assert(wattroff(w,A_BOLD | COLOR_PAIR(FOOTER_COLOR)) != ERR);
+	return OK;
+
+err:
+	return ERR;
+}
+
+static int
+setup_colors(void){
+	assert(init_pair(BORDER_COLOR,COLOR_GREEN,-1) == OK);
+	assert(init_pair(HEADER_COLOR,COLOR_BLUE,-1) == OK);
+	assert(init_pair(FOOTER_COLOR,COLOR_YELLOW,-1) == OK);
+	return 0;
+}
+
+static WINDOW *
+ncurses_setup(void){
+	const char *errstr = NULL;
+	WINDOW *w = NULL;
+
+	if(initscr() == NULL){
+		fprintf(stderr,"Couldn't initialize ncurses\n");
+		return NULL;
+	}
+	w = stdscr;
+	if(cbreak() != OK){
+		errstr = "Couldn't disable input buffering\n";
+		goto err;
+	}
+	if(noecho() != OK){
+		errstr = "Couldn't disable input echoing\n";
+		goto err;
+	}
+	if(intrflush(stdscr,TRUE) != OK){
+		errstr = "Couldn't set flush-on-interrupt\n";
+		goto err;
+	}
+	if(scrollok(stdscr,FALSE) != OK){
+		errstr = "Couldn't disable scrolling\n";
+		goto err;
+	}
+	if(nonl() != OK){
+		errstr = "Couldn't disable nl translation\n";
+		goto err;
+	}
+	if(start_color() != OK){
+		errstr = "Couldn't initialize ncurses color\n";
+		goto err;
+	}
+	if(use_default_colors()){
+		errstr = "Couldn't initialize ncurses colordefs\n";
+		goto err;
+	}
+	keypad(stdscr,TRUE);
+	if(nodelay(stdscr,FALSE) != OK){
+		errstr = "Couldn't set blocking input\n";
+		goto err;
+	}
+	if(setup_colors() != OK){
+		errstr = "Couldn't set up colors\n";
+		goto err;
+	}
+	if(curs_set(0) == ERR){
+		errstr = "Couldn't disable cursor\n";
+		goto err;
+	}
+	if(draw_main_window(w) != OK){
+		errstr = "Couldn't draw main window\n";
+		goto err;
+	}
+	refresh();
+	return w;
+
+err:
+	ncurses_cleanup(&w);
+	fprintf(stderr,"%s",errstr);
+	return NULL;
+}
+
+static void
+handle_ncurses_input(WINDOW *w){
+	int ch;
+
+	while((ch = getch()) != 'q' && ch != 'Q'){
+		switch(ch){
+			case 'h':{
+				// FIXME show help
+				wclear(w);
+				break;
+				 }
+
+		}
+	}
+}
+
 int main(int argc,char * const *argv){
+	WINDOW *w;
+
 	if(growlight_init(argc,argv)){
+		ncurses_cleanup(&w);
 		return EXIT_FAILURE;
 	}
-	// FIXME do stuff
+	if((w = ncurses_setup()) == NULL){
+		return EXIT_FAILURE;
+	}
+	handle_ncurses_input(w);
 	if(growlight_stop()){
+		ncurses_cleanup(&w);
+		return EXIT_FAILURE;
+	}
+	if(ncurses_cleanup(&w)){
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
