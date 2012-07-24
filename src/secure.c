4ff693baf0c09847b35409d11c1ea344d432eb69
nick black <nick.black@sprezzatech.com>
2012-07-23 23:10:53 -0400

    fprintf() removal, vspopen_drain()

--- src/secure.c
+++ src/secure.c
@@ -8,26 +8,16 @@
 #include "growlight.h"
 
 int ata_secure_erase(device *d){
-	char buf[PATH_MAX];
-
 	if(d->layout != LAYOUT_NONE){
-		fprintf(stderr,"Can only run ATA Erase on ATA-connected blockdevs\n");
+		diag("Can only run ATA Erase on ATA-connected blockdevs\n");
 		return -1;
 	}
-	if(snprintf(buf,sizeof(buf),"hdparm --user-master u --security-set-pass erasepw /dev/%s",d->name) >= (int)sizeof(buf)){
-		fprintf(stderr,"Bad pw name: %s\n",d->name);
+	if(vspopen_drain("hdparm --user-master u --security-set-pass erasepw /dev/%s",d->name)){
+		diag("Couldn't set ATA user password\n");
 		return -1;
 	}
-	if(popen_drain(buf)){
-		fprintf(stderr,"Couldn't set ATA user password\n");
-		return -1;
-	}
-	if(snprintf(buf,sizeof(buf),"hdparm --user-master u --security-erase erasepw /dev/%s",d->name) >= (int)sizeof(buf)){
-		fprintf(stderr,"Bad erase name: %s\n",d->name);
-		return -1;
-	}
-	if(popen_drain(buf)){
-		fprintf(stderr,"Couldn't perform ATA Secure Erase\n");
+	if(vspopen_drain("hdparm --user-master u --security-erase erasepw /dev/%s",d->name)){
+		diag("Couldn't perform ATA Secure Erase\n");
 		return -1;
 	}
 	return 0;
