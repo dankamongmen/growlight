394fc964b57f32e2cb0c033bf3adc084c5c2e0bc
nick black <nick.black@sprezzatech.com>
2012-08-14 08:13:15 -0400

    implement mmount() for mounting backend

--- src/mounts.h
+++ src/mounts.h
@@ -20,6 +20,7 @@ typedef struct mntentry {
 // (Re)parse the specified file having /proc/mounts format. Remember that
 // /proc/mounts must be poll()ed with POLLPRI, not POLLIN!
 int parse_mounts(const struct growlight_ui *,const char *);
+int mmount(struct device *,const char *,const char *);
 int unmount(struct device *);
 void clear_mounts(struct controller *);
 
