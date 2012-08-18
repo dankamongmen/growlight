39a7a62e19d533694dbfedd23d1de5be4cf07b06
nick black <nick.black@sprezzatech.com>
2012-08-18 08:13:49 -0400

    add more aggregate types

--- src/aggregate.c
+++ src/aggregate.c
@@ -3,27 +3,57 @@
 
 static const aggregate_type aggregates[] = {
 	{
-		.name = "linear",
-		.desc = "Linear disk combination",
+		.name = "mdlinear",
+		.desc = "Linear disk combination (MD)",
 	},{
-		.name = "raid0",
-		.desc = "Interleaved disk combination (striping)",
+		.name = "mdraid0",
+		.desc = "Interleaved disk combination (striping) (MD)",
 	},{
-		.name = "raid1",
-		.desc = "Mirroring",
+		.name = "mdraid1",
+		.desc = "Mirroring (MD)",
 	},{
-		.name = "raid4",
+		.name = "mdraid4",
 		.desc = "Block striping with dedicated parity",
 	},{
-		.name = "raid5",
+		.name = "mdraid5",
 		.desc = "Block striping with distributed parity",
 	},{
-		.name = "raid6",
+		.name = "mdraid6",
 		.desc = "Block striping with dual distributed parity",
 	},{
-		.name = "raid10",
+		.name = "mdraid10",
 		.desc = "Interleaved mirror combination",
-	},
+	},{
+		.name = "zmirror",
+		.desc = "Zpool with data replication (mirroring)",
+	},{
+		.name = "raidz1",
+		.desc = "ZFS RAID with single parity",
+	},{
+		.name = "raidz2",
+		.desc = "ZFS RAID with double parity",
+	},{
+		.name = "raidz3",
+		.desc = "ZFS RAID with triple parity",
+	},{
+		.name = "zil",
+		.desc = "ZFS Write-Intent Log",
+	},{
+		.name = "l2arc",
+		.desc = "ZFS Level 2 Adaptive Replacement Cache",
+	},{
+		.name = "dmlinear",
+		.desc = "Linear disk combination (DM)",
+	},{
+		.name = "dmstriped",
+		.desc = "Interleaved disk combination (striping) (DM)",
+	},{
+		.name = "crypt",
+		.desc = "Block encryption",
+	},{
+		.name = "dmmirror",
+		.desc = "Mirroring (DM)",
+	}
 };
 
 const aggregate_type *get_aggregate_types(int *count){
