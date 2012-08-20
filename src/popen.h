e47938a038947bc86380548de4c0617087f726a3
nick black <nick.black@sprezzatech.com>
2012-08-20 18:20:17 -0400

    kill vtpopen() and associated malarkey

--- src/popen.h
+++ src/popen.h
@@ -11,11 +11,6 @@ int popen_drain(const char *);
 int vpopen_drain(const char *,wchar_t * const *);
 int vspopen_drain(const char *,...) __attribute__ ((format (printf,1,2)));
 
-// Requires openvt. There are so many things wrong with this function I cannot
-// begin to enumerate them. FIXME
-int vtopen_drain(const char *);
-int vvtopen_drain(const char *,...) __attribute__ ((format (printf,1,2)));
-
 #ifdef __cplusplus
 }
 #endif
