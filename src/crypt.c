090ebc0198be48288746cb658fb3568e74185d84
nick black <nick.black@sprezzatech.com>
2012-10-02 06:30:19 -0400

    format the actual device

--- src/crypt.c
+++ src/crypt.c
@@ -5,6 +5,7 @@
 
 // Create LUKS on the device
 int cryptondev(device *d){
+	struct crypt_params_luks1 params;
 	struct crypt_device *cctx;
 	char path[PATH_MAX + 1];
 
@@ -16,6 +17,14 @@ int cryptondev(device *d){
 		diag("Couldn't create LUKS context for %s\n",d->name);
 		return -1;
 	}
+	memset(&params,0,sizeof(params));
+	params.hash = "sha1";
+	if(crypt_format(cctx,CRYPT_LUKS1,"aes","xts-plain64",NULL,NULL,256 / CHAR_BIT,&params)){
+		diag("Couldn't format LUKS on %s\n",d->name);
+		return -1;
+	}
+	// FIXME still need to set up keyslots and activate, including
+	// acquisition of a passphrase
 	crypt_free(cctx);
 	return 0;
 }
