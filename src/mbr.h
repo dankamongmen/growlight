7dadaf2b16fb449184f08a442b025ace691d0797
nick black <dankamongmen@gmail.com>
2019-08-03 21:38:58 -0400

    mbrsha1: print device name in errors

--- src/mbr.h
+++ src/mbr.h
@@ -5,14 +5,14 @@
 extern "C" {
 #endif
 
+struct device;
+
 // Take a SHA-1 checksum over the MBR code area. fd is an open fd for a true
 // block device. The buffer must be able to hold 20 bytes (160 bits). The
 // checksum is taken over the first 444 bytes, not all 512 bytes of the MBR.
-int mbrsha1(int,void *);
+int mbrsha1(struct device *, int, void *);
 
 int zerombrp(const void *);
-
-struct device;
 
 int wipe_biosboot(struct device *);
 int wipe_dosmbr(struct device *);
