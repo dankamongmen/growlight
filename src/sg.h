82a4995b2eddafe864a5be708fc488bf1277b16b
nick black <dankamongmen@gmail.com>
2019-08-03 22:13:40 -0400

    sg: extract cleanup_serial(), call from both sg/nvme (#6)

--- src/sg.h
+++ src/sg.h
@@ -5,10 +5,20 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
+
 struct device;
 
 // Takes an open file descriptor on the device node
-int sg_interrogate(struct device *,int);
+int sg_interrogate(struct device *, int);
+
+// Take the incoming serial number and trim leading, repeated, or trailing
+// whitespace. The serial number may or may not be NUL-terminated (don't blame
+// me; it's how the ioctls work). A NUL-terminator must be respected, but if
+// none is present in serialmax characters, stop. Returns a heap-allocated
+// buffer of not more than serialmax + 1 bytes, containing the NUL-terminated
+// and cleaned up serial number.
+void *cleanup_serial(const void *serial, size_t serialmax);
 
 #ifdef __cplusplus
 }
