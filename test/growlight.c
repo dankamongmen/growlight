04ae127a4676ce38a3d21ce59447465a7138816b
nick black <dankamongmen@gmail.com>
2019-08-07 21:32:11 -0400

    add unit testing infrastructure (#19)

--- test/growlight.c
+++ test/growlight.c
@@ -1,13 +1,52 @@
 #include <stdlib.h>
 #include <CUnit/Basic.h>
+#include "../src/growlight.h"
+
+static int
+init_suite(void) {
+	return 0;
+}
+
+static int
+clean_suite(void) {
+	return 0;
+}
+
+static void
+testGENPREFIX(void) {
+	char buf[PREFIXSTRLEN];
+
+	genprefix(0, 0, buf, sizeof(buf), 1, 1000, '\0');
+	CU_ASSERT_STRING_EQUAL(buf, "0");
+	genprefix(0, 0, buf, sizeof(buf), 1, 1000, 'i');
+	CU_ASSERT_STRING_EQUAL(buf, "0i");
+	// FIXME
+}
 
 int main() {
+	unsigned failures;
+	CU_pSuite suite;
 	if(CUE_SUCCESS != CU_initialize_registry()){
-		fprintf(stderr, "Couldn't initialize CUnit (%d)\n",
-				CU_get_error());
+		fprintf(stderr, "Couldn't initialize CUnit (%d)\n", CU_get_error());
 		exit(EXIT_FAILURE);
 	}
+	if((suite = CU_add_suite(__FILE__, init_suite, clean_suite)) == NULL){
+		fprintf(stderr, "Couldn't add CUnit suite %s (%d)\n",
+				__FILE__, CU_get_error());
+		exit(EXIT_FAILURE);
+	}
+	CU_add_test(suite, "genprefix()", testGENPREFIX);
 	CU_basic_set_mode(CU_BRM_VERBOSE);
-	CU_basic_run_tests();
+	if(CU_basic_run_tests()){
+		fprintf(stderr, "Error %d running CUnit tests\n", CU_get_error());
+		exit(EXIT_FAILURE);
+	}
+	failures = CU_get_number_of_failures();
+	if(failures){
+		fprintf(stderr, "Returning %d after %u test failure\n",
+				EXIT_FAILURE, failures);
+		exit(EXIT_FAILURE);
+	}
+	CU_cleanup_registry();
 	return CU_get_error();
 }
