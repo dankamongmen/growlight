1736333adfb1c116fc0c5bb316f3b3f639870a37
nick black <dankamongmen@gmail.com>
2020-11-01 04:44:08 -0500

    add recursive_lock_init() for glibc+musl #99

---
+++ src/threads.c
@@ -0,0 +1,26 @@
+// copyright 2012–2020 nick black
+#include "threads.h"
+
+// initialize a recursive mutex lock in a way that works on both glibc + musl
+int recursive_lock_init(pthread_mutex_t *lock){
+#ifndef __GLIBC__
+#define PTHREAD_MUTEX_RECURSIVE_NP PTHREAD_MUTEX_RECURSIVE
+#endif
+	pthread_mutexattr_t attr;
+	if(pthread_mutexattr_init(&attr)){
+		return -1;
+	}
+	if(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP)){
+		pthread_mutexattr_destroy(&attr);
+		return -1;
+	}
+	if(pthread_mutex_init(lock, &attr)){
+		pthread_mutexattr_destroy(&attr);
+		return -1;
+	}
+  pthread_mutexattr_destroy(&attr);
+	return 0;
+#ifndef __GLIBC__
+#undef PTHREAD_MUTEX_RECURSIVE_NP
+#endif
+}
---
+++ src/threads.h
@@ -0,0 +1,17 @@
+// copyright 2012–2020 nick black
+#ifndef GROWLIGHT_THREADS
+#define GROWLIGHT_THREADS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <pthread.h>
+
+int recursive_lock_init(pthread_mutex_t *lock);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
