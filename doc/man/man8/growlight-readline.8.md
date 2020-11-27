f30ab6529b810cc53665be822d3c0c9965bc5625
nick black <dankamongmen@gmail.com>
2020-11-27 12:31:23 -0500

    add skeleton for GPT CRC tests

--- doc/man/man8/growlight-readline.8.md
+++ doc/man/man8/growlight-readline.8.md
@@ -1,6 +1,6 @@
 % growlight-readline(8)
 % nick black <nickblack@linux.com>
-% v1.2.21
+% v1.2.22
 
 # NAME
 
---
+++ tests/gpt.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "gpt.h"
+
+TEST_CASE("GPT") {
+
+  SUBCASE("CRC32") {
+    update_crc(nullptr, nullptr);
+  }
+
+}
