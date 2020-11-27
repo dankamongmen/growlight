c59851556d8ef2ab298b851644d84d957fa3ae17
nick black <dankamongmen@gmail.com>
2020-11-27 04:55:55 -0500

    GPT unit test for zeroing out reserved section

--- tests/gpt.cpp
+++ tests/gpt.cpp
@@ -7,20 +7,27 @@ TEST_CASE("GPT") {
   // First eight bytes must be "EFI PART"
   SUBCASE("Signature") {
     gpt_header head;
-    initialize_gpt(&head, 0, 0, 0); // FIXME
-    CHECK(0 == memcmp(&head, "EFI PART", 8));
+    CHECK(0 == initialize_gpt(&head, 92, 0, 34));
+    CHECK(0 == memcmp(&head.signature, "EFI PART", sizeof(head.signature)));
   }
 
   // Bytes 0x8--0xb must be 00 00 01 00 (1.00 by UEFI logic)
   SUBCASE("Revision") {
     gpt_header head;
-    initialize_gpt(&head, 0, 0, 0); // FIXME
-    CHECK(0 == memcmp(reinterpret_cast<char*>(&head) + 8, "\x00\x00\x01\x00", 4));
+    CHECK(0 == initialize_gpt(&head, 92, 0, 34));
+    CHECK(0x00010000 == head.revision);
+  }
+
+  // Bytes 0xc--0xf must be >= 92, should be the logical block size
+  SUBCASE("HeaderSize") {
+    gpt_header head;
+    CHECK(0 == initialize_gpt(&head, 92, 0, 34));
+    CHECK(92 == head.headsize);
   }
 
   SUBCASE("CRC32") {
     gpt_header head;
-    initialize_gpt(&head, 0, 0, 0); // FIXME
+    CHECK(0 == initialize_gpt(&head, 92, 0, 34));
     // partition entry size must be a positive multiple of 128 (usually 128)
     CHECK(0 < head.partsize);
     CHECK(0 == (head.partsize % 128));
@@ -28,8 +35,25 @@ TEST_CASE("GPT") {
     CHECK(128 <= head.partcount);
     auto entries = new gpt_entry[head.partcount];
     update_crc(&head, entries);
-    // FIXME verify crc
+    CHECK(0xc803405b == head.crc);
+    CHECK(0x1ec50fb7 == head.partcrc);
     delete[] entries;
+  }
+
+  // Check that LBA of 512 sets header size and zeroes out remainder of sector.
+  // The Unified Extensible Firmware Interface Specification, Version 2.3.1,
+  // Errata C, June 27, 2012, states on page 104, in Table 16: "Size in bytes
+  // of the GPT Header. The HeaderSize must be greater than 92 and must be less
+  // than or equal to the logical block size." But everyone uses 92 :/.
+  SUBCASE("FullLBA") {
+    unsigned char sector[512];
+    memset(sector, 0xff, sizeof(sector));
+    gpt_header* head = reinterpret_cast<gpt_header*>(sector);
+    CHECK(0 == initialize_gpt(head, sizeof(sector), 0, 34));
+    CHECK(92 == head->headsize);
+    for(size_t idx = 92 ; idx < sizeof(sector) ; ++idx){
+      CHECK(0 == sector[idx]);
+    }
   }
 
 }
