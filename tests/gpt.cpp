428bc8e86e7dddc21c2011daa4eca5de10bdc6bd
nick black <dankamongmen@gmail.com>
2020-11-27 04:10:19 -0500

    unit test for gpt signature

--- tests/gpt.cpp
+++ tests/gpt.cpp
@@ -1,7 +1,15 @@
 #include "main.h"
 #include "gpt.h"
+#include <cstring>
 
 TEST_CASE("GPT") {
+
+  // First eight bytes must be "EFI PART"
+  SUBCASE("Signature") {
+    gpt_header head;
+    initialize_gpt(&head, 0, 0, 0); // FIXME
+    CHECK(0 == memcmp(&head, "EFI PART", 8));
+  }
 
   SUBCASE("CRC32") {
     gpt_header head;
