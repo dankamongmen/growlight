0835805b9d05359be5d2a04c28bfe3e273097556
nick black <dankamongmen@gmail.com>
2025-02-18 06:32:03 -0500

    gpt: kill dead store

--- src/ptypes.c
+++ src/ptypes.c
@@ -381,17 +381,17 @@ const ptype ptypes[] = {
 
 // Pass in the common code, get the scheme-specific identifier filled in.
 // Returns 0 for a valid code, or -1 if there's no ident for the scheme.
-int get_gpt_guid(unsigned code,void *guid){
+int get_gpt_guid(unsigned code, void *guid){
 	const ptype *pt;
 
 	for(pt = ptypes ; pt->name ; ++pt){
 		if(pt->code == code){
 			static const uint8_t zguid[GUIDSIZE] = {0};
 
-			if(memcmp(pt->gpt_guid,zguid,sizeof(zguid)) == 0){
+			if(memcmp(pt->gpt_guid, zguid, sizeof(zguid)) == 0){
 				return -1;
 			}
-			memcpy(guid,pt->gpt_guid,GUIDSIZE);
+			memcpy(guid, pt->gpt_guid, GUIDSIZE);
 			return 0;
 		}
 	}
