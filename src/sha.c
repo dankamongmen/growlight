c8fa605e60d1222afd73aa9179dae24b62097de4
nick black <dankamongmen@gmail.com>
2026-03-19 05:07:17 -0400

    work around nettle4 abi break
    nettle4 removes the length argument from its digest
    functions, resulting in a hard abi break. use nettle's
    version.h to check for 4+, and pass no length argument
    in this case.
    
    credit for patch due ametzler (Andreas Metzler) on github

--- src/sha.c
+++ src/sha.c
@@ -3,10 +3,15 @@
 
 #include "sha.h"
 #include "nettle/sha1.h"
+#include "nettle/version.h"
 
 void sha1(const void* src, size_t len, uint8_t* dst){
   struct sha1_ctx ctx;
   sha1_init(&ctx);
   sha1_update(&ctx, len, src);
+#if(NETTLE_VERSION_MAJOR >= 4)
+  sha1_digest(&ctx, dst);
+#else
   sha1_digest(&ctx, SHA1_DIGEST_SIZE, dst);
+#endif
 }
